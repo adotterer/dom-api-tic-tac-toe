281e78e8d7ed4038f1ff061b80676b1d2fccd229
Steve Guttbinder <sguttbinder@gmail.com>
2020-10-02 11:11:11 -0700

    Changes from 10-2 morning

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,28 +1,59 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  const currentPlayerSymbol = "x";
-  const squareValues = ["", "", "", "", "", "", "", "", ""];
-  const board = document.getElementById("tic-tac-toe-board");
+const currentPlayerSymbol = "x";
+const squareValues = ["", "", "", "", "", "", "", "", ""];
+const board = document.getElementById("tic-tac-toe-board");
+// Create global functions here so that listeners can call them below
+// const turnFinder = function (){
+//   let count = 0;
+//   squareValues.forEach(square => {
+//     if(square !== "") {
+//       count++
+//     }
+// if (count % 2 === 0){
+//   return true
+// } return false
+//   });
+  let count = 0;
+  squareValues.forEach(square => {
+    if (square !== ""){
+      count++;
+    }
+   
+  })
+   if (count % 2 === 0) {
+  console.log(true)
+
+} else {
+  console.log(false)
+}
+  
+  // If whole thing is empty, it's player 1
+  //  if there is an odd number of empty spaces = player 1
+// }
+window.addEventListener("DOMContentLoaded", (event) => { 
+  let currentPlayerSymbol = document.createElement("img" )
   board.addEventListener("click", (event) => {
     let target = event.target;
     let targetID = target.getAttribute("id");
+    let clickIndex = targetID.split("-")[1];
+  let turn = turnFinder()
+  console.log(turn);
+
     // let indexOfDash = targetID.indexOf("-");
     // let idNumber = targetID.slice(indexOfDash + 1);
-    if (!targetID.includes("square-")) {
+    if (!targetID.includes("square-") || squareValues[clickIndex] !== "" ) {
       console.log("Nothing happened!");
-    } else {
-      console.log(targetID.split("-")[1]);
+      return
+    } else{
+      if (turnFinder() === true ){
+        squareValues[clickIndex] = "X" 
+      } else{
+        squareValues[clickIndex] = "O" 
+      }
     }
+    console.log(squareValues)
   });
 });
 
-//     In the click event handler, inspect the target of the event.Ignore the event if
-// its id does not begin with "square-".
-
-// If the event target's id attribute does begin with "square-",
-// then parse the number after "square-" using the Number.parseInt method.
-
-// If the value in the squareValues array for the index of the
-// parsed number is not an empty string, then ignore the event.
 
 // There must not be a play in that square, so:
 // Programmatically create an img element, set its source to
